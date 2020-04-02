708fd941f18619ce5f987f73f6106acb49cd2a13
Rick Meels <rick.meels@gmail.com>
2020-04-02 13:20:01 +0200

    Added support to deploy docker image

--- src/main/java/com/copernicus/orsatellite/CopernicusApplication.java
+++ src/main/java/com/copernicus/orsatellite/CopernicusApplication.java
@@ -2,9 +2,20 @@ package com.copernicus.orsatellite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Controller
 public class CopernicusApplication {
+
+	@GetMapping("/")
+	public String index(final Model model) {
+		model.addAttribute("title", "Docker + Spring Boot");
+		model.addAttribute("msg", "Welcome to the docker container!");
+		return "index";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CopernicusApplication.class, args);
