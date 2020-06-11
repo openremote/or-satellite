123e0155a3994b79799d828e97e4ecd953b81b77
Luc Janssen <lphjanss@gmail.com>
2020-06-11 10:27:56 +0200

    Automated dataset download

---
+++ or-satellite/Models/SearchResultModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace or_satellite.Models
+{
+    public class SearchResultModel
+    {
+        public bool succes = false;
+        public string latitude;
+        public string longitude;
+        public string date;
+        public SearchResultEnum searchResult; 
+    }
+}
